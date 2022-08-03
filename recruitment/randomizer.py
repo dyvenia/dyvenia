80dc31add97631b131f62c009290dc814f2999aa
lzuchowska <zuc.ext@velux.com>
2022-08-03 14:24:53 +0200

    🐛 Moved connection to randomizer.py

--- recruitment/randomizer.py
+++ recruitment/randomizer.py
@@ -2,8 +2,14 @@ import sqlite3
 import random
 
 def connect_to_generator(task_level, task_type, amount):
-    con = sqlite3.connect('interview.db')
-    cur = con.cursor()
-    cur.execute(f"SELECT * FROM Tasks WHERE LEVEL == '{task_level}' AND TYPE == '{task_type}'")
-    tasks = cur.fetchall()
-    return random.sample(tasks, amount)
+    try:
+        con = sqlite3.connect('interview.db')
+        cur = con.cursor()
+        cur.execute(f"SELECT * FROM Tasks WHERE LEVEL == '{task_level}' AND TYPE == '{task_type}'")
+        tasks = cur.fetchall()
+        if amount > len(tasks):
+            return tasks
+        else: 
+            return random.sample(tasks, amount)
+    except ValueError:
+            pass
