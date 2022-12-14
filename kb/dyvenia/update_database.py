0d62ab89a73c239810796568e3580779d1f918d0
IrynaKovalchyk <irenkovalchik@gmail.com>
2022-12-14 09:52:28 +0100

    Modification of interview notebook, randomizer of question and update of data base due to change of structer

--- kb/dyvenia/update_database.py
+++ kb/dyvenia/update_database.py
@@ -1,19 +1,23 @@
 import sqlite3
 import csv
 
-con = sqlite3.connect("recruitment/interview.db")
+con = sqlite3.connect("interview.db")
 cur = con.cursor()
 
-table_exists = cur.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='Tasks'")
+table_exists = cur.execute(
+    "SELECT name FROM sqlite_master WHERE type='table' AND name='Tasks'"
+)
 
 if table_exists.fetchall():
     cur.execute("DROP TABLE Tasks")
 
-tasks_file = open("kb/dyvenia/InterviewTasks.csv")
+tasks_file = open("InterviewTasks.csv")
 task = csv.reader(tasks_file)
 
 try:
-    cur.execute("CREATE TABLE Tasks (LEVEL varchar(255), TYPE varchar(255), TASK varchar(1000))")
+    cur.execute(
+        "CREATE TABLE Tasks (ID int, CAT varchar(255), TASK varchar(1000))"
+    )
     cur.executemany("INSERT INTO Tasks VALUES (?, ?, ?)", task)
     if cur.rowcount > 0:
         print(f"Successfully inserted {cur.rowcount} rows!")
@@ -24,5 +28,3 @@ try:
         raise ValueError(f"Table was not updated.")
 except BaseException as e:
     print(f"Could not update the table. \nERROR: {e}")
-
-
